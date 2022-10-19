e4d140941d3efa6ca96855d98fc05469fb695366
Ankit kumar <105748411+sheraankit2112@users.noreply.github.com>
2022-10-19 17:55:15 +0530

    d

--- ankit/urls.py
+++ ankit/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.homepage,name="homepage"),
     path('signup',views.signuppage,name="signup"),
     path('login',views.loginpage,name="login"),
     path('afterlogin',views.afterlogin,name="afterlogin")
-]
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
